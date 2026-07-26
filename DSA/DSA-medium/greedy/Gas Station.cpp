56a290f1c057d367c56fe0ce397545ee86c78f2f
Duy Dat <duydat10082007@gmail.com>
2026-07-26 15:04:54 +0700

    Add comment exercise Gas Station

--- DSA/DSA-medium/greedy/Gas Station.cpp
+++ DSA/DSA-medium/greedy/Gas Station.cpp
@@ -4,22 +4,27 @@ using namespace std;
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0, tank = 0, start = 0;
-        for (int i = 0; i < (int)gas.size(); i++) {
-            int d = gas[i] - cost[i];
-            total += d;
-            tank += d;
-            if (tank < 0) {
-                start = i + 1;
-                tank = 0;
+        int xang_conlai = 0 ;
+        int xang_dadi = 0;
+        int start = 0;
+        for( int i=0 ; i< (int)gas.size() ; i++){
+            int temp_conlai = gas[i] - cost[i] ; // lượng xăng còn - lượng xăng phải sử dụng khi đi từ i->i+1
+            xang_conlai += temp_conlai ;
+            xang_dadi += temp_conlai ; 
+            if(xang_dadi < 0 ){ // bắt đầu từ vị trí này ko thể đi đc hết 1 vòng
+                start = i+1 ; // tăng vị trí bắt đầu lên 
+                xang_dadi = 0;  // bắt đầu lại từ đầu
             }
         }
-        return total >= 0 ? start : -1;
+        return xang_conlai >= 0 ? start : -1; // xăng còn lại có >=0 ko , có thì return start , ko thì -1
     }
 };
 
-
 int main(){
-    
+    vector<int> g= {2,3,4};
+    vector<int> c = {3,4,3};
+    Solution sol;
+    int result = sol.canCompleteCircuit(g,c);
+    cout << result << endl;
     return 0 ;
 }
