f84fa20ee4beaa52b2f8caa2012e513ddcf89ea2
Duy Dat <duydat10082007@gmail.com>
2026-08-05 17:49:39 +0700

    fix leetcode 721

--- DSA/Graph/DSU/721. Accounts Merge.cpp
+++ DSA/Graph/DSU/721. Accounts Merge.cpp
@@ -2,22 +2,42 @@
 using namespace std;
 
 class Solution {
-    vector<int> p ;
+    unordered_map<string , string> parent ;
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = (int)accounts.size() ;
-        unordered_map<string , int > m;
-        for(int i=0 ; i<n-1 ; i++)
-            for(int j=1 ; j<(int)accounts[i].size() ; j++){
-                string temp = accounts[i][j];
-                if(m.count(temp)) ; 
-                else m[temp] = i;
+        unordered_map<string,string > ower ; 
+        for(auto& acc : accounts){
+            string name = acc[0];
+            string fisrtemail = acc[1];
+            for(int i=1 ; i<(int)acc.size() ; i++){
+                string email = acc[i];
+                ower[email] = name ; // gắn tất cả email vào chung 1 tên chủ sở hữu tại i (ko phải tất cả )
+                string root1 = find(email); // tìm gốc của email
+                string root2 = find(fisrtemail);
+                if(root1 != root2) parent[root2] = root1 ;
+            //Nếu hai email đang thuộc hai nhóm khác nhau,thì nhập nhóm của email thứ hai vào nhóm của email thứ nhất, để chúng cùng một nhóm 
             }
-        return accounts;
+        }
+
+        unordered_map<string, vector<string>> groups;
+        for(auto& ow : ower){ // Gom email chung chủ sở hữu(chung gốc) về thành 1 cái 
+            string root = find(ow.first);
+            groups[root].push_back(ow.first); // chung root (gốc) thì đưa vào cùng 1 nhóm 
+        }
+        vector<vector<string>> ans ;
+        for( auto &g : groups){
+            auto &email = g.second ; // duyệt qua từng email ở từng nhóm 
+            sort(email.begin() , email.end() ); // sắp xếp
+            vector<string> acc = {ower[g.first]}; // lấy tên chủ sở hữu vì từ nãy h chỉ lấy email chứ ko đụng đến tên 
+            acc.insert(acc.end() , email.begin() , email.end()); // để tên ở đầu , push toàn bộ email chung chủ sở hữu vào cuối 
+            ans.push_back(acc);  // để vector<vector<string>> acc luôn cũng đc , đỡ tốn bộ nhớ cho ans 
+        }
+        return ans ;
     }
 
-    int find(int x){
-        return p[x] == x ? x : p[x] = find(p[x]);
+    string find(string email){
+        if( parent.find(email) == parent.end() ) parent[email] = email;
+        return parent[email] == email ? email:parent[email] = find(parent[email]);
     }
 };
 
@@ -29,10 +49,10 @@ int main(){
                                 {"John","johnnybravo@mail.com"}};
     Solution sol;
     vector<vector<string>> result = sol.accountsMerge(v);
-    for(int i=0 ; i<(int)result.size() ; i++){
-        for(int j=0 ; j<(int)result[i].size() ; j++)
-            cout << result[i][j] << " ; " ;
-        cout << endl ;
+    for(auto &acc:result){
+        for(auto &s:acc) 
+            cout<<s<<" ; ";
+        cout<<endl;
     }
     return 0 ;
 }
